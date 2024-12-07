b17cb2bc824a18a714b7afcd427405f04119c0eb
Alexis <alex6.treilles@gmail.com>
2024-12-07 12:49:14 +0100

    opti_gpt

--- opti_gpt.cpp
+++ opti_gpt.cpp
@@ -1,8 +1,7 @@
-#include <immintrin.h> // Pour AVX
+#include <immintrin.h>
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
 // Lire uniquement les dimensions de l'image
@@ -33,57 +32,46 @@ void write_img(const char *filename, unsigned char *image, int width, int height
     FILE *img = fopen(filename, "w");
     fprintf(img, "P1\n%d %d\n", width, height);
     for (int i = 0; i < width * height; i++) {
-        fprintf(img, "%d ", image[i]);
+        fprintf(img, "%d", image[i]);
+        if ((i + 1) % width == 0)
+            fprintf(img, "\n");
+        else
+            fprintf(img, " ");
     }
     fclose(img);
 }
 
-// Compter les voisins vivants avec AVX
-void count_live_neighbors_avx(unsigned char *current_image, int *neighbor_counts, int width, int height) {
+// Appliquer les règles du Jeu de la Vie
+void apply_game_of_life_avx(unsigned char *current_image, unsigned char *new_image, int width, int height) {
     #pragma omp parallel for
     for (int y = 1; y < height - 1; y++) {
-        for (int x = 1; x < width - 1; x += 8) { // Traite 8 pixels à la fois
-            __m256i neighbors = _mm256_setzero_si256();
+        for (int x = 1; x < width - 1; x += 32) { // AVX traite 32 pixels (256 bits)
+            __m256i live_neighbors = _mm256_setzero_si256();
 
-            // Charger les 8 voisins (en déroulant les décalages)
-            for (int dy = -1; dy <= 1; dy++) {
-                for (int dx = -1; dx <= 1; dx++) {
-                    if (dx == 0 && dy == 0) continue; // Ignorer le centre
-                    __m256i neighbor = _mm256_loadu_si256((__m256i *)&current_image[(y + dy) * width + (x + dx)]);
-                    neighbors = _mm256_add_epi32(neighbors, neighbor);
-                }
-            }
+            // Charger les 8 voisins
+            __m256i top = _mm256_loadu_si256((__m256i *)&current_image[(y - 1) * width + x]);
+            __m256i bottom = _mm256_loadu_si256((__m256i *)&current_image[(y + 1) * width + x]);
+            __m256i left = _mm256_loadu_si256((__m256i *)&current_image[y * width + x - 1]);
+            __m256i right = _mm256_loadu_si256((__m256i *)&current_image[y * width + x + 1]);
 
-            // Sauvegarder les résultats
-            _mm256_storeu_si256((__m256i *)&neighbor_counts[y * width + x], neighbors);
-        }
-    }
-}
+            live_neighbors = _mm256_add_epi8(live_neighbors, top);
+            live_neighbors = _mm256_add_epi8(live_neighbors, bottom);
+            live_neighbors = _mm256_add_epi8(live_neighbors, left);
+            live_neighbors = _mm256_add_epi8(live_neighbors, right);
 
-// Appliquer les règles du Jeu de la Vie avec AVX
-void apply_game_of_life_avx(unsigned char *current_image, unsigned char *new_image, int *neighbor_counts, int width, int height) {
-    #pragma omp parallel for
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x += 8) { // Traite 8 pixels à la fois
-            __m256i neighbors = _mm256_loadu_si256((__m256i *)&neighbor_counts[y * width + x]);
+            // Charger la cellule actuelle
             __m256i current = _mm256_loadu_si256((__m256i *)&current_image[y * width + x]);
 
-            // Cellules vivantes : (neighbors == 2 || neighbors == 3)
-            __m256i eq2 = _mm256_cmpeq_epi32(neighbors, _mm256_set1_epi32(2));
-            __m256i eq3 = _mm256_cmpeq_epi32(neighbors, _mm256_set1_epi32(3));
-            __m256i stay_alive = _mm256_or_si256(eq2, eq3);
+            // Règles : Reste vivante (2 ou 3 voisins), Devient vivante (exactement 3 voisins)
+            __m256i is_two = _mm256_cmpeq_epi8(live_neighbors, _mm256_set1_epi8(2));
+            __m256i is_three = _mm256_cmpeq_epi8(live_neighbors, _mm256_set1_epi8(3));
+            __m256i stay_alive = _mm256_and_si256(current, is_two);
+            __m256i become_alive = is_three;
 
-            // Cellules mortes : (neighbors == 3)
-            __m256i become_alive = _mm256_cmpeq_epi32(neighbors, _mm256_set1_epi32(3));
+            __m256i next_state = _mm256_or_si256(stay_alive, become_alive);
 
-            // Calcul final : vivante si vivante && stay_alive ou morte && become_alive
-            __m256i result = _mm256_or_si256(
-                _mm256_and_si256(current, stay_alive),
-                _mm256_andnot_si256(current, become_alive)
-            );
-
-            // Sauvegarder les résultats
-            _mm256_storeu_si256((__m256i *)&new_image[y * width + x], result);
+            // Sauvegarder le nouvel état
+            _mm256_storeu_si256((__m256i *)&new_image[y * width + x], next_state);
         }
     }
 }
@@ -91,31 +79,28 @@ void apply_game_of_life_avx(unsigned char *current_image, unsigned char *new_ima
 // Main
 int main() {
     int width, height;
-    int iterations = 100;
+    int iterations = 1000;
 
-    system("mkdir -p images"); // Créer le répertoire des images
+    system("mkdir -p images");
 
-    // Charger les dimensions et initialiser les images
     get_image_dimensions("image_init.ppm", &width, &height);
+
     unsigned char *current_image = (unsigned char *)aligned_alloc(32, width * height * sizeof(unsigned char));
     unsigned char *new_image = (unsigned char *)aligned_alloc(32, width * height * sizeof(unsigned char));
-    int *neighbor_counts = (int *)aligned_alloc(32, width * height * sizeof(int));
+
     open_img("image_init.ppm", current_image, width, height);
 
-    // Mesurer le temps
+    printf("Image loaded: width = %d, height = %d\n", width, height);
+
     clock_t start = clock();
 
-    // Générer les itérations
     for (int i = 0; i < iterations; i++) {
-        count_live_neighbors_avx(current_image, neighbor_counts, width, height);
-        apply_game_of_life_avx(current_image, new_image, neighbor_counts, width, height);
+        apply_game_of_life_avx(current_image, new_image, width, height);
 
-        // Sauvegarder l'image
         char filename[256];
         snprintf(filename, sizeof(filename), "images/frame_%04d.ppm", i);
         write_img(filename, new_image, width, height);
 
-        // Échanger les buffers
         unsigned char *temp = current_image;
         current_image = new_image;
         new_image = temp;
@@ -124,10 +109,8 @@ int main() {
     clock_t end = clock();
     printf("Temps total : %.2f secondes\n", (double)(end - start) / CLOCKS_PER_SEC);
 
-    // Libérer la mémoire
     free(current_image);
     free(new_image);
-    free(neighbor_counts);
 
     return 0;
 }
